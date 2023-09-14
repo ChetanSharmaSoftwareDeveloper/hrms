242b9ca193b5ba9c72e0f23a9aea2b4a5f4ec5ed
chetan <pcs1081999@gmail.com>
2023-09-14 13:06:52 +0530

    dashboard change

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,34 +5,42 @@
 <div class="row top_tiles">
     <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
         <div class="tile-stats">
-            <div class="icon"><i class="fa fa-caret-square-o-right"></i></div>
+            <div class="icon"><i class=""></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
-            <p>Lorem ipsum psdea itgum rixt.</p>
+            <h3>Total Employee</h3>
+            <p></p>
         </div>
     </div>
     <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
         <div class="tile-stats">
-            <div class="icon"><i class="fa fa-comments-o"></i></div>
+            <div class="icon"><i class=""></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
-            <p>Lorem ipsum psdea itgum rixt.</p>
+            <h3>Present Employee</h3>
+            <p></p>
         </div>
     </div>
     <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
         <div class="tile-stats">
-            <div class="icon"><i class="fa fa-sort-amount-desc"></i></div>
+            <div class="icon"><i class=""></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
-            <p>Lorem ipsum psdea itgum rixt.</p>
+            <h3>Absent Employee</h3>
+            <p></p>
         </div>
     </div>
     <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
         <div class="tile-stats">
-            <div class="icon"><i class="fa fa-check-square-o"></i></div>
+            <div class="icon"><i class=""></i></div>
             <div class="count">179</div>
-            <h3>New Sign ups</h3>
-            <p>Lorem ipsum psdea itgum rixt.</p>
+            <h3>Emplyee On Leave</h3>
+            <p></p>
+        </div>
+    </div>
+    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <div class="tile-stats">
+            <div class="icon"><i class=""></i></div>
+            <div class="count">179</div>
+            <h3>WFH On Employee</h3>
+            <p></p>
         </div>
     </div>
 </div>
